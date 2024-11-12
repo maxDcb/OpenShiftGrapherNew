468b5a8e02a8648e72b1b7fe6b4f603491467f43
Maxime DE CAUMIA BAILLENX <mdecaumiabaillenx@ncelrnd2669.nce.amadeus.net>
2024-11-12 14:09:20 +0100

    Fix OpenShiftGrapher & add tests folder

---
+++ tests/testOpenShiftGrapher.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest.mock import patch
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+
+if __name__ == "__main__":
+    unittest.main()
